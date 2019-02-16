6a306a91a2d42257effd2f7dfada0bff14da2502
Igor Chubin <igor@chub.in>
2019-02-16 20:53:18 +0100

    improved run-tests.sh; renamed tests (name+1)

---
+++ tests/results/19
@@ -0,0 +1,20 @@
+>>> s = 'abcdefgh'
+>>> n, m, char, chars = 2, 3, 'd', 'cd'
+>>> # starting from n=2 characters in and m=3 in length;
+>>> s[n-1:n+m-1]
+'bcd'
+>>> # starting from n characters in, up to the end of the string;
+>>> s[n-1:]
+'bcdefgh'
+>>> # whole string minus last character;
+>>> s[:-1]
+'abcdefg'
+>>> # starting from a known character char="d" within the string and of m length;
+>>> indx = s.index(char)
+>>> s[indx:indx+m]
+'def'
+>>> # starting from a known substring chars="cd" within the string and of m length.
+>>> indx = s.index(chars)
+>>> s[indx:indx+m]
+'cde'
+>>>
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,21 +2,37 @@
 
 TMP=$(mktemp /tmp/cht.sh.tests-XXXXXXXXXXXXXX)
 TMP2=$(mktemp /tmp/cht.sh.tests-XXXXXXXXXXXXXX)
-trap 'rm -rf $TMP $TMP2' EXIT
+TMP3=$(mktemp /tmp/cht.sh.tests-XXXXXXXXXXXXXX)
+trap 'rm -rf $TMP $TMP2 $TMP3' EXIT
 
+export CHTSH_URL=http://cht.sh:50000
+CHTSH_SCRIPT=$(dirname $(dirname $(readlink -f "$0")))/share/cht.sh.txt
 
 i=0
 failed=0
-while read -r test_line; do
+{
+  if [ -z "$1" ]; then
+    cat -n tests.txt
+  else
+    cat -n tests.txt | sed -n "$(echo $* | sed 's/ /p; /g;s/$/p/')"
+  fi
+} > "$TMP3"
+while read -r number test_line; do
+  number=$((number-1))
   test_line="${test_line// #.*//}"
-  eval "$test_line" > "$TMP"
-  diff results/"$i" "$TMP" > "$TMP2"
+  if [[ $test_line = "cht.sh "* ]]; then
+    test_line="${test_line//cht.sh /}"
+    eval "bash $CHTSH_SCRIPT $test_line" > "$TMP"
+  else
+    eval "curl -s $CHTSH_URL/$test_line" > "$TMP"
+  fi
+  diff results/"$number" "$TMP" > "$TMP2"
   if [ "$?" != 0 ]; then
-    echo FAILED: [$i] $test_line
+    echo "FAILED: [$number] $test_line"
     ((failed++))
   fi
   ((i++))
-done < tests2.txt
+done < "$TMP3"
 
 echo TESTS/OK/FAILED "$i/$[i-failed]/$failed"
 
