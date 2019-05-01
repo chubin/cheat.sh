22dfb74f72aaed5b1e86a4e1611bd1b18e670ad8
Igor Chubin <igor@chub.in>
2019-05-01 09:30:56 +0200

    lib/adapter/__init__.py: load adapter modules dynamically

--- lib/adapter/__init__.py
+++ lib/adapter/__init__.py
@@ -1,9 +1,18 @@
-from . import cheat_sheets
-from . import cmd
-from . import internal
-from . import latenz
-from . import learnxiny
-from . import question
-from . import rosetta
-from . import cheat_cheat
-from . import tldr
+"""
+Import all adapters from the current directory
+and make them available for import as
+    adapter_module.AdapterName
+"""
+
+# pylint: disable=wildcard-import,relative-import
+
+from os.path import dirname, basename, isfile, join
+import glob
+
+__all__ = [
+    basename(f)[:-3]
+    for f in glob.glob(join(dirname(__file__), "*.py"))
+    if isfile(f) and not f.endswith('__init__.py')]
+
+from adapter import all_adapters
+from . import *
