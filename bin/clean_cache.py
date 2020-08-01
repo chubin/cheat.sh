b74707a109749eb680f754c7f1d4f31e7a18956e
Anatoli Babenia <anatoli@rainforce.org>
2020-08-01 06:10:03 +0300

    Redis doesn't need to be Strict
    https://github.com/andymccurdy/redis-py#client-classes-redis-and-strictredis

--- bin/clean_cache.py
+++ bin/clean_cache.py
@@ -1,6 +1,6 @@
 import sys
 import redis
-REDIS = redis.StrictRedis(host='localhost', port=6379, db=0)
+REDIS = redis.Redis(host='localhost', port=6379, db=0)
 
 for key in sys.argv[1:]:
     REDIS.delete(key)
